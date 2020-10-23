2f12c5dd0c0ba9bfab9be7cb9360df26eb223187
Suranjan Rai <Suranjanthulungrai@gmail.com>
2020-10-23 10:37:06 +0545

    Updates

---
+++ precache-manifest.0f1dcb180092cf5a6b9d59a29044c46f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349d4bf3f806c53f6438aedf00224845",
+    "url": "/SSPRM/index.html"
+  },
+  {
+    "revision": "54cc892e69a93fc6b609",
+    "url": "/SSPRM/static/css/main.2896b56e.chunk.css"
+  },
+  {
+    "revision": "d8b55e48f9bfe490b221",
+    "url": "/SSPRM/static/js/2.d6b42d1d.chunk.js"
+  },
+  {
+    "revision": "9fda8197ca6e25979aa843f42451bfaf",
+    "url": "/SSPRM/static/js/2.d6b42d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54cc892e69a93fc6b609",
+    "url": "/SSPRM/static/js/main.26a934c8.chunk.js"
+  },
+  {
+    "revision": "4de9ac24dc17a0af6c5e",
+    "url": "/SSPRM/static/js/runtime-main.693ab325.js"
+  },
+  {
+    "revision": "05fa336788efd016001ca8c18972b8de",
+    "url": "/SSPRM/static/media/SSPRM NEON-main.05fa3367.png"
+  },
+  {
+    "revision": "a3ede17115797d947564a1db44e5c7d4",
+    "url": "/SSPRM/static/media/about-section.a3ede171.jpg"
+  },
+  {
+    "revision": "811e9593df9b1e4b2a5f2bb10f9f929e",
+    "url": "/SSPRM/static/media/back.811e9593.jpg"
+  },
+  {
+    "revision": "89cf4ebbc65765e8800d5097a4e68a23",
+    "url": "/SSPRM/static/media/dark.89cf4ebb.jpg"
+  }
+]);
